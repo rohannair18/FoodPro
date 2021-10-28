35f5fb6d9c7b8670e5789fb950a3810831585db1
Rohan <34743825+rohannair18@users.noreply.github.com>
2021-10-28 18:43:40 +0530

    Commit 1

--- app/src/main/java/com/example/foodpro/Food.java
+++ app/src/main/java/com/example/foodpro/Food.java
@@ -7,7 +7,7 @@ public class Food {
     String s_address;
     String s_password;
 
-    public Food(String sName, String s_name, String s_email, String s_address, String s_place) {
+    public Food( String s_name, String s_email, String s_address, String s_password, String s_username) {
         this.s_name = s_name;
         this.s_email = s_email;
         this.s_address = s_address;
