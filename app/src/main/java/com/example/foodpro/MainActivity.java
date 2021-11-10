0d8d519094d3069090d6676567f9e97d6234accf
Rohan <34743825+rohannair18@users.noreply.github.com>
2021-11-10 13:49:14 +0530

    Commit 1

--- app/src/main/java/com/example/foodpro/MainActivity.java
+++ app/src/main/java/com/example/foodpro/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.foodpro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,11 +26,16 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     Button btn1;
     FirebaseAuth mAuth;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth=FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
+        username=findViewById(R.id.Username);
+        password=findViewById(R.id.Password);
+
         btn=(Button)findViewById(R.id.register);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +48,30 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent j = new Intent(getApplicationContext(), Restaurants.class);
-                startActivity(j);
+                //Intent j = new Intent(getApplicationContext(), Restaurants.class);
+                //startActivity(j);
+                String s_username,s_password;
+                s_username=username.getText().toString().trim();
+                s_password=password.getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(s_username,s_password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(MainActivity.this, Restaurants.class);
+                                    startActivity(i);
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
             }
         });
 
